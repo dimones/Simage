209391f748b640e77682cf1afc0076ab0958e6ee
Dmitriy <Dimones951@yandex.ru>
2014-05-26 17:52:13 +0700

    Hackaton release

---
+++ TEMP/TEMP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TEMP
+//
+//  Created by Dmitriy Bogomolov on 25.05.14.
+//  Copyright (c) 2014 Dimones-dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TEMP/TEMP/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TEMP
+//
+//  Created by Dmitriy Bogomolov on 25.05.14.
+//  Copyright (c) 2014 Dimones-dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
